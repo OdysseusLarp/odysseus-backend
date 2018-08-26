89997558c80fced273ea81a31099800290bdeda7
Nico Hagelberg <nico.hagelberg@gmail.com>
2018-08-26 03:55:11 +0300

    test: add initial tests for emptyepsilon client

---
+++ test/emptyepsilon.js
@@ -0,0 +1,114 @@
+import test from 'ava';
+import sinon from 'sinon';
+import nock from 'nock';
+import EmptyEpsilonClient from '../src/emptyepsilon';
+
+// Disable real http communications
+nock.disableNetConnect();
+
+// Line below can be used to record actual http requests so they can turned into mocks
+// nock.recorder.rec();
+
+test('should set state to healthy', async t => {
+	nock('http://localhost:8080', { "encodedQueryParams": true })
+		.get('/get.lua')
+		.query(true)
+		.reply(200, {}, []);
+	const emptyEpsilon = new EmptyEpsilonClient();
+	const gameState = await emptyEpsilon.getGameState();
+	t.truthy(emptyEpsilon.isConnectionHealthy);
+});
+
+test('should set state to unhealthy', async t => {
+	nock('http://localhost:8080', { "encodedQueryParams": true })
+		.get('/get.lua')
+		.query(true)
+		.reply(500, { 'ERROR': 'mock error' },
+			[
+				'Content-type',
+				'text/html',
+				'Connection',
+				'Keep-Alive',
+				'Transfer-Encoding',
+				'chunked'
+			]);
+	const emptyEpsilon = new EmptyEpsilonClient();
+	const gameState = await emptyEpsilon.getGameState();
+	t.falsy(emptyEpsilon.isConnectionHealthy);
+});
+
+test('should parse getGameState reponse correctly', async t => {
+	nock('http://localhost:8080', { "encodedQueryParams": true })
+		.get('/get.lua')
+		.query(true)
+		.reply(200, {
+			"reactorHeat": 0,
+			"homingCount": 12,
+			"nukeCount": 4,
+			"frontshieldHealth": 1,
+			"rearshieldHeat": 0,
+			"missilesystemHeat": 0,
+			"maneuverHeat": 0,
+			"jumpdriveHealth": 1,
+			"beamweaponsHeat": 0,
+			"frontshieldHeat": 0,
+			"hvliCount": 20,
+			"impulseHeat": 0,
+			"jumpdriveHeat": 0,
+			"rearshieldHealth": 1,
+			"warpHeat": 0,
+			"impulseHealth": 1,
+			"missilesystemHealth": 1,
+			"reactorHealth": 1,
+			"maneuverHealth": 1,
+			"mineCount": 8,
+			"warpHealth": 1,
+			"beamweaponsHealth": 1,
+			"empCount": 6
+		},
+			[
+				'Content-type',
+				'text/html',
+				'Connection',
+				'Keep-Alive',
+				'Transfer-Encoding',
+				'chunked'
+			]);
+	const emptyEpsilon = new EmptyEpsilonClient();
+	const gameState = await emptyEpsilon.getGameState();
+	t.truthy(emptyEpsilon.isConnectionHealthy);
+	t.deepEqual(gameState, {
+		systems: {
+			heat: {
+				reactorHeat: 0,
+				rearshieldHeat: 0,
+				missilesystemHeat: 0,
+				maneuverHeat: 0,
+				beamweaponsHeat: 0,
+				frontshieldHeat: 0,
+				impulseHeat: 0,
+				jumpdriveHeat: 0,
+				warpHeat: 0
+			},
+			health: {
+				frontshieldHealth: 1,
+				jumpdriveHealth: 1,
+				rearshieldHealth: 1,
+				impulseHealth: 1,
+				missilesystemHealth: 1,
+				reactorHealth: 1,
+				maneuverHealth: 1,
+				warpHealth: 1,
+				beamweaponsHealth: 1
+			}
+		},
+		weapons: {
+			homingCount: 12,
+			nukeCount: 4,
+			hvliCount: 20,
+			mineCount: 8,
+			empCount: 6
+		}
+	})
+});
+
