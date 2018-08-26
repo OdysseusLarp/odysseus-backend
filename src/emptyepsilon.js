ce7acba8d03abd86e89f2eee938547f6b58184be
Nico Hagelberg <nico.hagelberg@gmail.com>
2018-08-26 03:54:15 +0300

    refactor: convert emptyepsilon client to a class

--- src/emptyepsilon.js
+++ src/emptyepsilon.js
@@ -3,57 +3,84 @@ import logger from 'signale';
 import axios from 'axios';
 import { get, set } from 'lodash';
 
-const { EMPTY_EPSILON_HOST, EMPTY_EPSILON_PORT } = process.env;
-const URL = `http://${EMPTY_EPSILON_HOST}:${EMPTY_EPSILON_PORT}`;
-const GET_URL = `${URL}/get.lua`;
-const SET_URL = `${URL}/set.lua`;
+const { EMPTY_EPSILON_HOST, EMPTY_EPSILON_PORT } = process.env,
+	systems = [
+		'reactor',
+		'beamweapons',
+		'missilesystem',
+		'maneuver',
+		'impulse',
+		'warp',
+		'jumpdrive',
+		'frontshield',
+		'rearshield'
+	],
+	weapons = ['homing', 'nuke', 'mine', 'emp', 'hvli'];
 
-const systems = [
-  'reactor',
-  'beamweapons',
-  'missilesystem',
-  'maneuver',
-  'impulse',
-  'warp',
-  'jumpdrive',
-  'frontshield',
-  'rearshield'
-];
-const weapons = ['homing', 'nuke', 'mine', 'emp', 'hvli'];
+export default class EmptyEpsilonClient {
+	constructor({ host = EMPTY_EPSILON_HOST, port = EMPTY_EPSILON_PORT } = {}) {
+		const url = `http://${host}:${port}`;
+		this.getUrl = `${url}/get.lua`;
+		this.setUrl = `${url}/set.lua`;
+	}
 
-const createRequestParameters = configs =>
-  configs.reduce((previous, next) => 
-    next.targets.reduce((p, n) => {
-      const obj = Object.assign({}, p);
-      obj[n + next.keySuffix] = `${next.command}(\"${n}\")`;
-      return obj;
-    }, previous), {});
+	setIsConnectionHealthy(value, error) {
+		const errorMessage = get(error, 'message');
+		const { isConnectionHealthy, lastErrorMessage } = this;
+		if (value === isConnectionHealthy && errorMessage === lastErrorMessage) return;
+		if (value) {
+			logger.success('Connection to Empty Epsilon succeeded');
+		} else {
+			logger.error('Connection to Empty Epsilon failed:', errorMessage);
+		}
+		this.lastErrorMessage = errorMessage;
+		this.isConnectionHealthy = value;
+	}
 
-const requestParameters = createRequestParameters([
-  { keySuffix: 'Count', command: 'getWeaponStorage', targets: weapons },
-  { keySuffix: 'Health', command: 'getSystemHealth', targets: systems },
-  { keySuffix: 'Heat', command: 'getSystemHeat', targets: systems }
-]);
+	getGameState() {
+		return axios.get(this.getUrl, { params: requestParameters }).then((res, err) => {
+			// Handle connection errors
+			if (err) throw new Error(err);
+			// Handle Empty Epsilon response errors that use inconsistent keys
+			const emptyEpsilonError = get(res, 'data.ERROR') || get(res, 'data.error');
+			if (emptyEpsilonError) throw new Error(emptyEpsilonError);
+			// Otherwise assume valid connection
+			this.setIsConnectionHealthy(true);
+			return Object.keys(res.data).reduce((data, key) => {
+				let keyPrefix;
+				if (key.includes('Health')) keyPrefix = 'systems.health';
+				else if (key.includes('Heat')) keyPrefix = 'systems.heat';
+				return set(data, `${keyPrefix || 'weapons'}.${key}`, res.data[key]);
+			}, {});
+		}).catch(error => {
+			this.setIsConnectionHealthy(false, error);
+			return { error };
+		});
+	};
 
-export const setGameState = (command, target, value) => {
-  // Only allow known commands to be used
-  if (!['setSystemHealth', 'setSystemHeat', 'setWeaponStorage'].includes(command))
-    return Promise.reject('Invalid command', command);
-  return axios.get(`${SET_URL}?${command}("${target}",${value})`).then(res => {
-    if (get(res, 'data.ERROR')) throw new Error(res.data.ERROR);
-    logger.success('Empty Epsilon game state set', command, target, value);
-    return res;
-  });
+	setGameState(command, target, value) {
+		// Only allow known commands to be used
+		if (!['setSystemHealth', 'setSystemHeat', 'setWeaponStorage'].includes(command))
+			return Promise.reject('Invalid command', command);
+		return axios.get(`${this.setUrl}?${command}("${target}",${value})`).then(res => {
+			if (get(res, 'data.ERROR')) throw new Error(res.data.ERROR);
+			logger.success('Empty Epsilon game state set', command, target, value);
+			return res;
+		});
+	}
 }
 
-export const getGameState = () => {
-  return axios.get(GET_URL, { params: requestParameters }).then((res, err) => {
-    if (err) throw new Error(err);
-    return Object.keys(res.data).reduce((data, key) => {
-      let keyPrefix;
-      if (key.includes('Health')) keyPrefix = 'systems.health';
-      else if (key.includes('Heat')) keyPrefix = 'systems.heat';
-      return set(data, `${keyPrefix || 'weapons'}.${key}`, res.data[key]);
-    }, {});
-  });
-};
+function createRequestParameters(configs) {
+	return configs.reduce((previous, next) =>
+		next.targets.reduce((p, n) => {
+			const obj = Object.assign({}, p);
+			obj[n + next.keySuffix] = `${next.command}(\"${n}\")`;
+			return obj;
+		}, previous), {});
+}
+
+const requestParameters = createRequestParameters([
+	{ keySuffix: 'Count', command: 'getWeaponStorage', targets: weapons },
+	{ keySuffix: 'Health', command: 'getSystemHealth', targets: systems },
+	{ keySuffix: 'Heat', command: 'getSystemHeat', targets: systems }
+]);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 import logger from 'signale';
 import { get } from 'lodash';
-import { getGameState, setGameState } from './emptyepsilon';
+import EmptyEpsilonClient from './emptyepsilon';
+
+const emptyEpsilon = new EmptyEpsilonClient();
 
 // Setup logging middleware
 app.use((req, res, next) => {
@@ -13,13 +15,13 @@ app.use((req, res, next) => {
 });
 
 // Temporary store for game state
-let state = { };
+let state = {};
 
 app.get('/', (req, res) => res.send('Hello'));
 app.get('/state', (req, res) => res.json(state));
 app.get('/state/:command/:target/:value', (req, res) => {
 	const { command, target, value } = req.params;
-	setGameState(command, target, value)
+	emptyEpsilon.setGameState(command, target, value)
 		.then(() => res.status(200).send('ok'))
 		.catch(error => {
 			logger.error('Error setting game state', error);
@@ -39,14 +41,15 @@ const { APP_PORT } = process.env;
 http.listen(APP_PORT, () => logger.start(`Odysseus backend listening to port ${APP_PORT}`));
 
 // Get Empty Epsilon game state and emit it to clients
-const updateGameState = async () => {
-	// TODO: Handle errors
-	// TODO: Validate state and make sure that EE mission did not just change
-	state = await getGameState();
-	io.emit('gameStateUpdated', state);
-	logger.log('Empty Epsilon game state updated');
-}
-
 const EE_UPDATE_INTERVAL = 1000;
+let isEmptyEpsilonConnectionHealthy;
 logger.watch(`Starting to poll Empty Epsilon game state every ${EE_UPDATE_INTERVAL}ms`);
-setInterval(updateGameState, EE_UPDATE_INTERVAL);
+setInterval(() => {
+	// TODO: Validate state and make sure that EE mission did not just change
+	emptyEpsilon.getGameState()
+		.then(state => {
+			if (state.error) return;
+			io.emit('gameStateUpdated', state);
+			logger.info('Empty Epsilon game state updated');
+		});
+}, EE_UPDATE_INTERVAL);
