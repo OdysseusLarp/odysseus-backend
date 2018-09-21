f0813a11ce814a839a440bd5492f2165a0314db7
Nico Hagelberg <nico.hagelberg@gmail.com>
2018-09-21 23:56:13 +0300

    chore: add virtuals plugin

--- db/index.js
+++ db/index.js
@@ -2,5 +2,7 @@ import knexConfig from '../knexfile';
 
 const knex = require('knex')(knexConfig);
 
-/* eslint-disable-next-line no-unused-vars */
-const Bookshelf = module.exports = require('bookshelf')(knex);
+
+const Bookshelf = require('bookshelf')(knex);
+Bookshelf.plugin('virtuals');
+module.exports = Bookshelf;
