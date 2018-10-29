266f75f3fcdb195a5eccdc9190d7e61235f021d8
Nico Hagelberg <nico.hagelberg@gmail.com>
2018-10-29 19:53:30 +0200

    feat: add working box upsert and update routes

--- src/routes/engineering.js
+++ src/routes/engineering.js
@@ -1,15 +1,23 @@
+import BigNumber from 'bignumber.js';
 import { Router } from 'express';
 import { Task } from '../models/task';
+import { Box } from '../models/box';
 import { handleAsyncErrors } from '../helpers';
+import { logger } from '../logger';
 const router = new Router();
 
-router.get('/', handleAsyncErrors(async (req, res) => {
+const VALIDATE_BOX_VERSIONS = !process.env.DISABLE_BOX_VERSION_VALIDATION;
+if (!VALIDATE_BOX_VERSIONS) {
+	logger.warn('Engineering Box versions will NOT be validated on update');
+}
+
+router.get('/task', handleAsyncErrors(async (req, res) => {
 	const tasks = await Task.forge().fetchAll();
 	const state = { tasks };
 	res.json(state);
 }));
 
-router.get('/:id', handleAsyncErrors(async (req, res) => {
+router.get('/task/:id', handleAsyncErrors(async (req, res) => {
 	const { id } = req.params;
 	const tasks = await Task.forge({ id }).fetch();
 	const state = { tasks };
@@ -17,40 +25,30 @@ router.get('/:id', handleAsyncErrors(async (req, res) => {
 }));
 
 router.get('/box', handleAsyncErrors(async (req, res) => {
-	// const state = await Box.forge().fetchAll({
-	// 	withRelated: 'tasks'
-	// });
-	const state = {};
-	res.json(state);
+	res.json(await Box.forge().fetchAll());
 }));
 
 router.get('/box/:id', handleAsyncErrors(async (req, res) => {
-	const { id } = req.params;
-	// const state = await Box.forge({ id }).fetch({
-	// 	withRelated: 'tasks'
-	// });
-	const state = {};
-	res.json(state);
+	res.json(await Box.forge({ id: req.params.id }).fetch());
 }));
 
-router.get('/box/:id/config', handleAsyncErrors(async (req, res) => {
-	const { id } = req.params;
-	// TODO: Get box config
-	const state = { id };
-	res.json(state);
-}));
-
+// Upsert for box values
 router.post('/box/:id', handleAsyncErrors(async (req, res) => {
-	const { id } = req.params;
-	const { value } = req.body;
-	// TODO: Validate value from request body
-	// TODO: Validate new value against task requirements attached to this box
-	await Box.forge({ id }).save({ value: { value } }, { method: 'update' });
-	const state = await Box.forge({ id }).fetch({
-		withRelated: 'tasks'
-	});
-	res.json(state);
-	req.io.to('engineering').emit('boxStateUpdated', state);
+	const { id } = req.params, { value, version } = req.body;
+	const newVersion = new BigNumber(version || 1);
+	// TODO: Validate value & version from request body
+	let box = await Box.forge({ id }).fetch();
+	if (!box) box = await Box.forge().save(
+		{ id, value, version: newVersion.toString() }, { method: 'insert' });
+	else {
+		const currentVersion = BigNumber(box.get('version'));
+		if (VALIDATE_BOX_VERSIONS && newVersion.isLessThanOrEqualTo(currentVersion)) {
+			throw new Error(`Currently on version number ${currentVersion.toString()}`);
+		}
+		await box.save({ value, version: newVersion.toString() }, { method: 'update' });
+	}
+	res.json(box);
+	req.io.to('engineering').emit('boxStateUpdated', box);
 }));
 
 export default router;
