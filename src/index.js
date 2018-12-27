10fe5675223d670e36b2a84f083c80a99f00e889
Kalle Kivimaa <killer@basen.net>
2018-12-27 21:25:45 +0200

    feat: simple GET and possibly even POST for infoboard

--- src/index.js
+++ src/index.js
@@ -61,6 +61,7 @@ app.put('/state', setStateRouteHandler);
 app.use('/data', data);
 app.use('/infoboard', infoboard);
 app.use('/data', data);
+app.use('/infoboard', infoboard);
 
 // Error handling middleware
 app.use(async (err, req, res, next) => {
