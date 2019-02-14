a4e2fcebccde04e03c85e150cf36f3a8a9243266
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-02-14 20:54:53 +0200

    chore: run eslint --fix

--- src/routes/data.js
+++ src/routes/data.js
@@ -6,63 +6,63 @@ const router = new Router();
 
 
 function getData(dataType, dataId) {
-    const state = store.getState().data;
-    if (state[dataType] && state[dataType][dataId]) {
-        return state[dataType][dataId];
-    } else {
-        return {};
-    }
+	const state = store.getState().data;
+	if (state[dataType] && state[dataType][dataId]) {
+		return state[dataType][dataId];
+	} else {
+		return {};
+	}
 }
 
-function setData(dataType, dataId, data, force=false) {
-    if (!force) {
-        const oldData = getData(dataType, dataId);
-        if (oldData.version && oldData.version !== data.version) {
-            throw new httpErrors.Conflict(`Wrong version number, provided '${data.version}', expected '${oldData.version}'`);
-        }
-    }
+function setData(dataType, dataId, data, force = false) {
+	if (!force) {
+		const oldData = getData(dataType, dataId);
+		if (oldData.version && oldData.version !== data.version) {
+			throw new httpErrors.Conflict(`Wrong version number, provided '${data.version}', expected '${oldData.version}'`);
+		}
+	}
 
-    store.dispatch({
-      type: 'SET_DATA',
-      dataType,
-      dataId,
-      data,
-    });
+	store.dispatch({
+		type: 'SET_DATA',
+		dataType,
+		dataId,
+		data,
+	});
 }
 
 
 /**
  * Get all data types in the store.
- * 
+ *
  * @route GET /data
  * @group Data - Generic data store operations
  * @returns {Object} 200 - Array of current data types
  */
 router.get('/', (req, res) => {
-    const state = store.getState().data;
-    res.json(Object.keys(state));
+	const state = store.getState().data;
+	res.json(Object.keys(state));
 });
 
 
 /**
  * Get all data blogs by type.
- * 
+ *
  * @route GET /data/{type}
  * @group Data - Generic data store operations
  * @param {string} type.path.required - Data type
  * @returns {Object} 200 - Array of data blobs
  */
 router.get('/:type', (req, res) => {
-    const { type } = req.params;
-    const state = store.getState().data;
-    const datas = state[type] || {};
-    res.json(Object.values(datas));
+	const { type } = req.params;
+	const state = store.getState().data;
+	const datas = state[type] || {};
+	res.json(Object.values(datas));
 });
 
 
 /**
  * Get a specific data blob by type and id.
- * 
+ *
  * @route GET /data/{type}/{id}
  * @group Data - Generic data store operations
  * @param {string} id.path.required - Data id
@@ -70,16 +70,16 @@ router.get('/:type', (req, res) => {
  * @returns {Object} 200 - Data value
  */
 router.get('/:type/:id', (req, res) => {
-    const { type, id } = req.params;
-    return res.json(getData(type, id));
+	const { type, id } = req.params;
+	return res.json(getData(type, id));
 });
 
 
 /**
  * Set a specific data blob by type and id. Overwrites entire data.
- * 
+ *
  * Returns 409 if version is incorrect and force not used.
- * 
+ *
  * @route POST /data/{type}/{id}
  * @consumes application/json
  * @group Data - Generic data store operations
@@ -91,19 +91,19 @@ router.get('/:type/:id', (req, res) => {
  * @returns {Error}  409 - Error if submitted version is different that current version
  */
 router.post('/:type/:id', (req, res) => {
-    const { type, id } = req.params;
-    const { force } = req.query;
-    const data = req.body;
-    setData(type, id, data, force);
-    res.json(getData(type, id));
+	const { type, id } = req.params;
+	const { force } = req.query;
+	const data = req.body;
+	setData(type, id, data, force);
+	res.json(getData(type, id));
 });
 
 
 /**
  * Update a specific data blob by type and id. Keeps those fields which are not present in payload.
- * 
+ *
  * Returns 409 if version is incorrect and force not used.
- * 
+ *
  * @route PATCH /data/{type}/{id}
  * @consumes application/json
  * @group Data - Generic data store operations
@@ -115,11 +115,11 @@ router.post('/:type/:id', (req, res) => {
  * @returns {Error}  409 - Error if submitted version is different that current version
  */
 router.patch('/:type/:id', (req, res) => {
-    const { type, id } = req.params;
-    const { force } = req.query;
-    const data = {...getData(type, id), ...{version: undefined}, ...req.body};
-    setData(type, id, data, force);
-    res.json(getData(type, id));
+	const { type, id } = req.params;
+	const { force } = req.query;
+	const data = { ...getData(type, id), ...{ version: undefined }, ...req.body };
+	setData(type, id, data, force);
+	res.json(getData(type, id));
 });
 
 
