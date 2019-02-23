4d9c263b49edfc9ae66752810b05a68a6df860a1
Kalle Kivimaa <killer@basen.net>
2019-02-23 13:47:00 +0200

    Working backend timer

--- src/routes/infoboard.js
+++ src/routes/infoboard.js
@@ -1,16 +1,40 @@
 import { Router } from 'express';
 import { InfoEntry } from '../models/infoentry';
 import { handleAsyncErrors } from '../helpers';
+import { logger, loggerMiddleware } from '../logger';
+
 const router = new Router();
 
 /**
- * Get a list of all infoboard entries
+ * Get a single infoboard entry to display
  * @route GET /infoboard
  * @group Infoboard - Infoboard related operations
- * @returns {Array.<InfoEntry>} 200 - List of all infoboard entries
+ * @returns {<InfoEntry>} 200 - An infoboard entry
  */
 router.get('/', handleAsyncErrors(async (req, res) => {
-	res.json(await InfoEntry.forge().fetchAll());
+    const now = new Date();
+    const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5);
+    const entries = await InfoEntry.forge().where({enabled: true}).orderBy('priority', 'DESC').fetchAll();
+    let entry = null;
+    let sum = 0;
+    for( let e in entries.models ) {
+	const attributes = entries.models[e].attributes;
+	sum += parseInt(attributes.priority);
+    }
+    const realSelector = selector % sum;
+    let currentSum = 0;
+    for( let e in entries.models ) {
+	const attributes = entries.models[e].attributes;
+	currentSum += parseInt(attributes.priority);
+	if( realSelector < currentSum ) {
+	    entry = entries.models[e];
+	    break;
+	}
+    }
+    
+    //const entry = entries.models[selector % 2];
+    logger.debug('Loaded ' + entry + ' at ' + selector + ' from ' + entries.length + ' as ' + sum);
+	res.json(entry);
 }));
 
 /**
