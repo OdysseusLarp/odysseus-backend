c06c1427a98faf7a26f5324f602bebb5e8e993cc
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-02-25 17:53:34 +0200

    chore: fix formatting

--- src/store/reducers/dataReducer.js
+++ src/store/reducers/dataReducer.js
@@ -1,39 +1,39 @@
-import { createReducer } from 'redux-starter-kit'
+import { createReducer } from 'redux-starter-kit';
 
 const dataReducer = createReducer({}, {
-    "SET_DATA" : (state, action) => {
-        const id = action.dataId;
-        const type = action.dataType;
+	SET_DATA: (state, action) => {
+		const id = action.dataId;
+		const type = action.dataType;
 
-        state[type] = state[type] || {};
-        const oldData = state[type][id] || {};
-        const newData = {...action.data};
-        
-        newData.id = id;
-        newData.type = type;
-        if (oldData.created_at) {
-            newData.created_at = oldData.created_at;
-        } else {
-            newData.created_at = new Date().toISOString();
-        }
-        newData.updated_at = new Date().toISOString();
-        if (typeof oldData.version !== 'number') {
-            newData.version = 1;
-        } else {
-            newData.version = oldData.version + 1;
-        }
+		state[type] = state[type] || {};
+		const oldData = state[type][id] || {};
+		const newData = { ...action.data };
 
-        state[type][id] = newData;
-    },
+		newData.id = id;
+		newData.type = type;
+		if (oldData.created_at) {
+			newData.created_at = oldData.created_at;
+		} else {
+			newData.created_at = new Date().toISOString();
+		}
+		newData.updated_at = new Date().toISOString();
+		if (typeof oldData.version !== 'number') {
+			newData.version = 1;
+		} else {
+			newData.version = oldData.version + 1;
+		}
 
-    "DELETE_DATA": (state, action) => {
-        const id = action.dataId;
-        const type = action.dataType;
+		state[type][id] = newData;
+	},
 
-        if (state[type]) {
-            delete state[type][id];
-        }
-    }
-})
+	DELETE_DATA: (state, action) => {
+		const id = action.dataId;
+		const type = action.dataType;
 
-export default dataReducer
+		if (state[type]) {
+			delete state[type][id];
+		}
+	}
+});
+
+export default dataReducer;
--- src/store/storeSocket.js
+++ src/store/storeSocket.js
@@ -4,53 +4,51 @@ import { logger } from '../logger';
 
 let previousData;
 function sendDataChanges(io) {
-    const currentData = store.getState().data;
+	const currentData = store.getState().data;
 
-    for (let type of Object.keys(currentData)) {
-        for (let id of Object.keys(currentData[type])) {
-            if (!previousData[type] || previousData[type][id] !== currentData[type][id]) {
-                // console.log(`Firing /data/${type}/${id} dataUpdate events`)
-                io.to(`/data/${type}/${id}`).emit('dataUpdate', type, id, currentData[type][id]);
-                io.to(`/data/${type}`).emit('dataUpdate', type, id, currentData[type][id]);
-                io.to(`/data`).emit('dataUpdate', type, id, currentData[type][id]);
-            }
-        }
-    }
+	for (const type of Object.keys(currentData)) {
+		for (const id of Object.keys(currentData[type])) {
+			if (!previousData[type] || previousData[type][id] !== currentData[type][id]) {
+				io.to(`/data/${type}/${id}`).emit('dataUpdate', type, id, currentData[type][id]);
+				io.to(`/data/${type}`).emit('dataUpdate', type, id, currentData[type][id]);
+				io.to(`/data`).emit('dataUpdate', type, id, currentData[type][id]);
+			}
+		}
+	}
 
-    for (let type of Object.keys(previousData)) {
-        for (let id of Object.keys(previousData[type])) {
-            if (!currentData[type] || !currentData[type][id]) {
-                // console.log(`Firing /data/${type}/${id} dataDelete events`)
-                io.to(`/data/${type}/${id}`).emit('dataDelete', type, id);
-                io.to(`/data/${type}`).emit('dataDelete', type, id);
-                io.to(`/data`).emit('dataDelete', type, id);
-            }
-        }
-    }
+	for (const type of Object.keys(previousData)) {
+		for (const id of Object.keys(previousData[type])) {
+			if (!currentData[type] || !currentData[type][id]) {
+				io.to(`/data/${type}/${id}`).emit('dataDelete', type, id);
+				io.to(`/data/${type}`).emit('dataDelete', type, id);
+				io.to(`/data`).emit('dataDelete', type, id);
+			}
+		}
+	}
 
-    previousData = currentData;
+	previousData = currentData;
 }
 const throttledSendDataChanges = throttle(sendDataChanges, 10, { leading: false, trailing: true });
 
 
 export function initStoreSocket(io) {
 
-    // Use /data namespace and 'room' query parameter to subscribe
-    var nsp = io.of('/data');
-    nsp.on('connection', function(socket) {
-        var room = socket.handshake['query']['data'] || '/data';
+	// Use /data namespace and 'room' query parameter to subscribe
+	const nsp = io.of('/data');
+	nsp.on('connection', socket => {
+		const room = socket.handshake['query']['data'] || '/data';
 
-        socket.join(room);
-        logger.info(`Socket.io listening to data changes for '${room}'`)
-      
-        socket.on('disconnect', function() {
-          socket.leave(room)
-          logger.info(`Socket.io disconnected from '${room}'`)
-        });
-    });
+		socket.join(room);
+		logger.info(`Socket.io listening to data changes for '${room}'`);
 
-    previousData = store.getState().data;
-    store.subscribe(() => {
-        throttledSendDataChanges(nsp);
-    });
+		socket.on('disconnect', () => {
+		  socket.leave(room);
+		  logger.info(`Socket.io disconnected from '${room}'`);
+		});
+	});
+
+	previousData = store.getState().data;
+	store.subscribe(() => {
+		throttledSendDataChanges(nsp);
+	});
 }
