c9aab19a52ff66398a422503723a065d36063bab
Sampo Juustila <sampo.juustila@iki.fi>
2019-03-02 20:57:11 +0200

    chore: Add helper functions to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -28,4 +28,28 @@ store.subscribe(() => {
 	throttledSaveState(data, 'data');
 });
 
+export function getPath(path) {
+	let o = store.getState();
+	while (path && path.length > 0) {
+		if (o[path[0]]) {
+			o = o[path[0]];
+		} else {
+			return undefined;
+		}
+		path = path.slice(1)
+	}
+	return o;
+}
+
+export function watch(path, callback) {
+	let previousObject = getPath(path);
+	store.subscribe(() => {
+		const newObject = getPath(path);
+		if (newObject !== previousObject) {
+			callback(newObject, previousObject);
+			previousObject = newObject;
+		}
+	});
+}
+
 export default store;
