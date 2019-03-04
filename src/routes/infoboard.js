ddc4922d5d4bd4ea47b1ca1f3090e8b373a2800c
Kalle Kivimaa <killer@basen.net>
2019-03-04 17:08:10 +0200

    Fix next message pickup logic

--- src/routes/infoboard.js
+++ src/routes/infoboard.js
@@ -12,28 +12,28 @@ const router = new Router();
  * @returns {<InfoEntry>} 200 - An infoboard entry
  */
 router.get('/', handleAsyncErrors(async (req, res) => {
-    const now = new Date();
-    const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5);
-    const entries = await InfoEntry.forge().where({enabled: true}).orderBy('priority', 'DESC').fetchAll();
-    let entry = null;
-    let sum = 0;
-    for( let e in entries.models ) {
-	const attributes = entries.models[e].attributes;
-	sum += parseInt(attributes.priority);
-    }
-    const realSelector = selector % sum;
-    let currentSum = 0;
-    for( let e in entries.models ) {
-	const attributes = entries.models[e].attributes;
-	currentSum += parseInt(attributes.priority);
-	if( realSelector < currentSum ) {
-	    entry = entries.models[e];
-	    break;
+	const now = new Date();
+	const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5);
+	const entries = await InfoEntry.forge().where({enabled: true}).orderBy('priority', 'DESC').fetchAll();
+	let entry = null;
+	let sum = 0;
+	for( let e in entries.models ) {
+		const attributes = entries.models[e].attributes;
+		sum += parseInt(attributes.priority);
 	}
-    }
+	const realSelector = selector % sum;
+	let currentSum = 0;
+	for( let e in entries.models ) {
+		const attributes = entries.models[e].attributes;
+		currentSum += parseInt(attributes.priority);
+		if( realSelector < currentSum ) {
+			entry = entries.models[e];
+			break;
+		}
+	}
     
     //const entry = entries.models[selector % 2];
-    logger.debug('Loaded ' + entry + ' at ' + selector + ' from ' + entries.length + ' as ' + sum);
+	logger.debug('Loaded ' + entry + ' at ' + selector + ' from ' + entries.length + ' as ' + sum);
 	res.json(entry);
 }));
 
