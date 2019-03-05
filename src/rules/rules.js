f57d5cbe85e0fbd8fb91a10930bf9ae108e51779
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-03-05 07:38:51 +0200

    fix: prevent application from crashing if redux store is empty

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -1,10 +1,11 @@
 import path from 'path'
 import fs from 'fs'
+import { isFunction } from 'lodash';
 
 /* Require all *.js files in all subdirectories */
 
 fs.readdirSync(__dirname, { withFileTypes:true }).forEach((entry) => {
-    if (entry.isDirectory()) {
+    if (isFunction(entry.isDirectory) && entry.isDirectory()) {
         const dir = path.join(__dirname, entry.name);
         fs.readdirSync(dir).forEach((file) => {
             if (file.endsWith('.js')) {
