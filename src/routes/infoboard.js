3a3250d459d19188824166976a81b62895f3027b
Kalle Kivimaa <killer@basen.net>
2019-03-05 17:36:09 +0200

    Functional backend

--- src/routes/infoboard.js
+++ src/routes/infoboard.js
@@ -14,31 +14,31 @@ const router = new Router();
  * @returns {<InfoEntry>} 200 - An infoboard entry
  */
 router.get('/', handleAsyncErrors(async (req, res) => {
-    const event = await Event.forge().where({ship_id: 'odysseus', is_active: true, type: 'JUMP'}).fetch();
+	const event = await Event.forge().where({ship_id: 'odysseus', is_active: true, type: 'JUMP'}).fetch();
 	const now = new Date();
 	const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5);
 	const priority = await InfoPriority.forge().fetch();
-    const entries = await InfoEntry.forge().where({priority: priority.attributes.priority}).fetchAll();
+	const entries = await InfoEntry.forge().where({priority: priority.attributes.priority}).fetchAll();
 	const count = entries.length;
-    const realSelector = selector % count;
-    let entry = entries.models[realSelector];
-    if( event ) {
-	let seconds = parseInt((event.attributes.occurs_at.getTime() - now.getTime()) / 1000);
-	const hours = parseInt(seconds / 3600);
-	const minutes = parseInt((seconds - hours * 3600) / 60);
-	seconds = seconds % 60;
-	let time = seconds + " seconds";
-	if( minutes > 0 ) {
-	    time = minutes + " minutes, " + time;
+	const realSelector = selector % count;
+	let entry = entries.models[realSelector];
+	if( event ) {
+		let seconds = parseInt((event.attributes.occurs_at.getTime() - now.getTime()) / 1000);
+		const hours = parseInt(seconds / 3600);
+		const minutes = parseInt((seconds - hours * 3600) / 60);
+		seconds = seconds % 60;
+		let time = seconds + " seconds";
+		if( minutes > 0 ) {
+			time = minutes + " minutes, " + time;
+		}
+		if( hours > 0 ) {
+			time = hours + " hours, " + time;
+		}
+		if( seconds <  0 || minutes < 0 || hours < 0 ) {
+			time = "JUMPING NOW";
+		}
+		entry.attributes.time_to_jump = time;
 	}
-	if( hours > 0 ) {
-	    time = hours + " hours, " + time;
-	}
-	if( seconds <  0 || minutes < 0 || hours < 0 ) {
-	    time = "JUMPING NOW";
-	}
-	entry.attributes.time_to_jump = time;
-    }
 	res.json(entry);
 }));
 
