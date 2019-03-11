31ef865baadadcd3b158c23ae6376e79d2572813
Kalle Kivimaa <killer@basen.net>
2019-03-11 21:58:36 +0200

    Fix seeds for better current usage

--- db/seeds/06-infoboard.js
+++ db/seeds/06-infoboard.js
@@ -3,31 +3,31 @@ const infos = [
 	priority: 1,
 	enabled: true,
 	title: 'Test slide 1',
-	body: '<b>Simple bold text line</b>'
+	body: '<b>Lorem ipsum dolor sit amet, an mei liber timeam regione, enim viris invenire vis et. Dolor blandit expetendis pro at. Graeci conceptam dissentiet in duo. Ut lorem augue audiam quo.</b>'
     },
     {
 	priority: 1,
 	enabled: true,
 	title: 'Test slide 2',
-	body: '<p>Just regular text</p>'
+	body: '<p>An sonet scripserit eum, id malorum expetenda consetetur usu. Persius quaestio an nec, partem complectitur te duo. Ut petentium corrumpit qui, novum epicuri scribentur sed ei, dicit viderer ut duo. Ex summo qualisque eum, sit viris ceteros definitiones et. Sed ut vero legere nostrum, ius rationibus constituam instructior ei.</p><p>Pro ei nisl nostro quaestio, mucius adipiscing concludaturque eum cu, eos mutat sapientem efficiendi cu. Luptatum probatus maluisset eu usu, duo ignota tincidunt ea, sit agam inani homero ad. Assum regione ad eum, vix cu dicit iisque tincidunt. Per facete euripidis ei, no decore nostrum adipiscing pri, pri et posse omnes mediocrem. Persequeris liberavisse cum at, ad sit hinc nominavi eloquentiam, ridens oblique explicari an sed.</p>'
     },
     {
-	priority: 2,
+	priority: 7,
 	enabled: true,
-	title: 'Higher priority test slide 1',
-	body: '<p>Just regular text but at higher priority.</p>'
+	title: 'Jump prep started',
+	body: '<p text-align="center">The ship is now preparing for a jump.</p>'
     },
     {
-	priority: 2,
+	priority: 8,
 	enabled: true,
-	title: 'Higher priority test slide 2',
-	body: '<b>Just some bold text at higher priority.</b>'
+	title: 'Jump prep completed',
+	body: '<b text-align="center">The ship is now ready to jump at a short notice.</b>'
     },
     {
-	priority: 3,
+	priority: 9,
 	enabled: true,
-	title: 'Top priority slide',
-	body: '<b>This will not change.</b>'
+	title: 'Jump sequence initiated',
+	body: '<b text-align="center">Jump in %%JUMP%% seconds.</b>'
     },
 ];
 
