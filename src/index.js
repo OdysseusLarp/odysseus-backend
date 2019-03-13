b178dca8ac87728bf9878fc41d506fd7059fe9a4
Sampo Juustila <sampo.juustila@iki.fi>
2019-03-13 15:44:01 +0200

    feat: Add express-admin to /database

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import { loadEvents } from './eventhandler';
 import { loadMessaging } from './messaging';
 import { Store } from './models/store';
 import cors from 'cors';
+import fs from 'fs';
+import expressAdmin from 'express-admin';
 
 import { initStoreSocket } from './store/storeSocket';
 import { initState } from './store/store';
@@ -29,85 +31,106 @@ import infoboard from './routes/infoboard';
 
 import './rules/rules';
 
-// Setup logging middleware and body parsing
-app.use(bodyParser.json());
-app.use(loggerMiddleware);
-app.use(cors());
-
 // Temporary store for game state
 export let gameState = {};
 
-// Add Socket.IO reference to all requests so route handlers can call it
-app.use((req, res, next) => {
-	req.io = io;
-	next();
-});
+var config = {
+	dpath: './express-admin-config/',
+	config: JSON.parse(fs.readFileSync('express-admin-config/config.json')),
+	settings: JSON.parse(fs.readFileSync('express-admin-config/settings.json')),
+	custom: JSON.parse(fs.readFileSync('express-admin-config/custom.json')),
+	users: JSON.parse(fs.readFileSync('express-admin-config/users.json'))
+	// additionally you can pass your own session middleware to use
+	// session: session({...})
+};
 
-// Setup routes
-app.get('/', (req, res) => res.redirect('/api-docs'));
-app.use('/engineering', engineering);
-app.use('/fleet', fleet);
-app.use('/starmap', starmap);
-app.use('/person', person);
-app.use('/event', event);
-app.use('/post', post);
-app.use('/vote', vote);
-app.use('/log', log);
-app.use('/science', science);
-app.get('/state', (req, res) => res.json(gameState));
-app.put('/state', setStateRouteHandler);
-app.use('/data', data);
-app.use('/infoboard', infoboard);
+expressAdmin.init(config, function(err, admin) {
+	if (err) return console.log(err);
 
-// Error handling middleware
-app.use(async (err, req, res, next) => {
-	logger.error(err.message);
-	return res.status(err.status || 500).json({ error: err.message });
-});
+	app.use('/database', admin);
+	// site specific middlewares
 
-// Setup Socket.IO
-io.on('connection', socket => {
-	logger.info('Socket.IO Client connected');
-	socket.on('disconnect', () => {
-		logger.info('Socket.IO Client disconnected');
+	// Setup logging middleware and body parsing
+	app.use(bodyParser.json());
+	app.use(loggerMiddleware);
+	app.use(cors());
+
+	// Add Socket.IO reference to all requests so route handlers can call it
+	app.use((req, res, next) => {
+		req.io = io;
+		next();
 	});
-});
-loadMessaging(io);
 
-// Get Empty Epsilon game state and emit it to clients
-function getEmptyEpsilonState() {
-	// TODO: Validate state and make sure that EE mission did not just change
-	getEmptyEpsilonClient().getGameState().then(state => {
-		if (state.error) return;
-		gameState = state;
-		io.emit('gameStateUpdated', state);
+	// Setup routes
+	app.get('/', (req, res) => res.redirect('/api-docs'));
+	app.use('/engineering', engineering);
+	app.use('/fleet', fleet);
+	app.use('/starmap', starmap);
+	app.use('/person', person);
+	app.use('/event', event);
+	app.use('/post', post);
+	app.use('/vote', vote);
+	app.use('/log', log);
+	app.use('/science', science);
+	app.get('/state', (req, res) => res.json(gameState));
+	app.put('/state', setStateRouteHandler);
+	app.use('/data', data);
+	app.use('/infoboard', infoboard);
+
+	// Error handling middleware
+	app.use(async (err, req, res, next) => {
+		logger.error(err.message);
+		return res.status(err.status || 500).json({ error: err.message });
 	});
-}
 
-// Load initial engineering tasks and current events
-loadInitialTasks();
-loadEvents(io);
+	// Setup Socket.IO
+	io.on('connection', socket => {
+		logger.info('Socket.IO Client connected');
+		socket.on('disconnect', () => {
+			logger.info('Socket.IO Client disconnected');
+		});
+	});
+	loadMessaging(io);
 
-const EE_UPDATE_INTERVAL = 1000;
-logger.watch(`Starting to poll Empty Epsilon game state every ${EE_UPDATE_INTERVAL}ms`);
-setInterval(getEmptyEpsilonState, EE_UPDATE_INTERVAL);
+	// Get Empty Epsilon game state and emit it to clients
+	function getEmptyEpsilonState() {
+		// TODO: Validate state and make sure that EE mission did not just change
+		getEmptyEpsilonClient()
+			.getGameState()
+			.then(state => {
+				if (state.error) return;
+				gameState = state;
+				io.emit('gameStateUpdated', state);
+			});
+	}
 
-// Load initial state from database before starting the HTTP listener
-Store.forge({ id: 'data' }).fetch().then(model => {
-	const data = model ? model.get('data') : {};
-	initState(data);
-	logger.info('State initialized');
-	startServer();
+	// Load initial engineering tasks and current events
+	loadInitialTasks();
+	loadEvents(io);
+
+	const EE_UPDATE_INTERVAL = 1000;
+	logger.watch(`Starting to poll Empty Epsilon game state every ${EE_UPDATE_INTERVAL}ms`);
+	setInterval(getEmptyEpsilonState, EE_UPDATE_INTERVAL);
+
+	// Load initial state from database before starting the HTTP listener
+	Store.forge({ id: 'data' })
+		.fetch()
+		.then(model => {
+			const data = model ? model.get('data') : {};
+			initState(data);
+			logger.info('State initialized');
+			startServer();
+		});
+
+	// Generate and serve API documentation using Swagger at /api-docs
+	loadSwagger(app);
+
+	initStoreSocket(io);
+
+	function startServer() {
+		const { APP_PORT } = process.env;
+		http.listen(APP_PORT, () => logger.start(`Odysseus backend listening to port ${APP_PORT}`));
+	}
 });
-
-// Generate and serve API documentation using Swagger at /api-docs
-loadSwagger(app);
-
-initStoreSocket(io);
-
-function startServer() {
-	const { APP_PORT } = process.env;
-	http.listen(APP_PORT, () => logger.start(`Odysseus backend listening to port ${APP_PORT}`));
-}
 
 export { app };
