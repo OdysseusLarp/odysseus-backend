2458437c29da9b8c26e14efec143536d85517e4c
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-04-04 22:37:06 +0300

    fix: update route jsdoc to fix social hub apigen build issue, add parseInt radix, fix imports

--- src/routes/infoboard.js
+++ src/routes/infoboard.js
@@ -3,40 +3,40 @@ import { Event } from '../models/event';
 import { InfoEntry } from '../models/infoentry';
 import { InfoPriority } from '../models/infoentry';
 import { handleAsyncErrors } from '../helpers';
-import { logger, loggerMiddleware } from '../logger';
 import Bookshelf from '../../db';
+import httpErrors from 'http-errors';
 
 const router = new Router();
 
-let timeTo = function(time, prefix, expired) {
-	let seconds = parseInt((time.getTime() - (new Date()).getTime()) / 1000);
-	const hours = parseInt(seconds / 3600);
-	const minutes = parseInt((seconds - hours * 3600) / 60);
-	seconds = seconds % 60;
-	let text = seconds + " seconds";
-	if( minutes > 0 ) {
-		text = minutes + " minutes, " + text;
+const timeTo = function (time, prefix, expired) {
+	let seconds = parseInt((time.getTime() - (new Date()).getTime()) / 1000, 10);
+	const hours = parseInt(seconds / 3600, 10);
+	const minutes = parseInt((seconds - hours * 3600) / 60, 10);
+	seconds %= 60;
+	let text = `${seconds} seconds`;
+	if ( minutes > 0 ) {
+		text = `${minutes} minutes, ${text}`;
 	}
-	if( hours > 0 ) {
-		text = hours + " hours, " + text;
+	if ( hours > 0 ) {
+		text = `${hours} hours, ${text}`;
 	}
 	text = prefix + text;
-	if( seconds <  0 || minutes < 0 || hours < 0 ) {
+	if ( seconds <  0 || minutes < 0 || hours < 0 ) {
 		text = expired;
 	}
 	return text;
-}
+};
 
 /**
  * Update current priority
  * @route PUT /infoboard/priority
  * @consumes application/json
  * @group Infoboard - Infoboard related operations
- * @param {integer} priority.required - New priority to set
+ * @param {number} priority.body.required - New priority to set
  * @returns {object} 200 - Empty response on success
  */
 router.put('/priority', handleAsyncErrors(async (req, res) => {
-	await Bookshelf.knex('infoboard_priority').where('priority', '>', 0).update({priority: req.body.priority});
+	await Bookshelf.knex('infoboard_priority').where('priority', '>', 0).update({ priority: req.body.priority });
 	res.sendStatus(200);
 }));
 
@@ -47,21 +47,21 @@ router.put('/priority', handleAsyncErrors(async (req, res) => {
  * @returns {<InfoEntry>} 200 - An infoboard entry
  */
 router.get('/display', handleAsyncErrors(async (req, res) => {
-	const jumpEvent = await Event.forge().where({ship_id: 'odysseus', is_active: true, type: 'JUMP'}).fetch();
-	const prepEvent = await Event.forge().where({ship_id: 'odysseus', is_active: true, type: 'JUMP_PREP'}).fetch();
+	const jumpEvent = await Event.forge().where({ ship_id: 'odysseus', is_active: true, type: 'JUMP' }).fetch();
+	const prepEvent = await Event.forge().where({ ship_id: 'odysseus', is_active: true, type: 'JUMP_PREP' }).fetch();
 	const now = new Date();
-	const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5);
+	const selector = parseInt((now.getMinutes() * 60 + now.getSeconds()) / 5, 10);
 	const priority = await InfoPriority.forge().fetch();
-	const entries = await InfoEntry.forge().where({priority: priority.attributes.priority}).fetchAll();
+	const entries = await InfoEntry.forge().where({ priority: priority.attributes.priority }).fetchAll();
 	const count = entries.length;
 	const realSelector = selector % count;
-	let entry = entries.models[realSelector];
-	if( prepEvent ) {
-		let time = timeTo(prepEvent.attributes.occurs_at, "Ready to jump in ", "Ready to jump.");
+	const entry = entries.models[realSelector];
+	if ( prepEvent ) {
+		const time = timeTo(prepEvent.attributes.occurs_at, 'Ready to jump in ', 'Ready to jump.');
 		entry.attributes.jump_text = time;
 	}
-	if( jumpEvent ) {
-		let time = timeTo(jumpEvent.attributes.occurs_at, "Jump in ", "Jumping now!");
+	if ( jumpEvent ) {
+		const time = timeTo(jumpEvent.attributes.occurs_at, 'Jump in ', 'Jumping now!');
 		entry.attributes.jump_text = time;
 	}
 	res.json(entry);
@@ -85,7 +85,7 @@ router.get('/enabled', handleAsyncErrors(async (req, res) => {
  */
 router.get('/', handleAsyncErrors(async (req, res) => {
 	const priority = await InfoPriority.forge().fetch();
-    res.json({priority: priority, infoboards: await InfoEntry.forge().fetchAll()});
+	res.json({ priority, infoboards: await InfoEntry.forge().fetchAll() });
 }));
 
 /**
