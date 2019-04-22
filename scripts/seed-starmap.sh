2eab1fccd6ac633e418541cce802715789e632c2
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-04-22 17:35:45 +0300

    chore: add script for seeding starmap using psql

---
+++ scripts/seed-starmap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATA_DIR="$DIR/../db/data"
+
+source "$DIR/../.env"
+
+# Insert data from CSVs
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME \
+	-c "\copy starmap_bg FROM '$DATA_DIR/starmap_bg.csv' DELIMITER ',' CSV HEADER" \
+	-c "\copy starmap_object FROM '$DATA_DIR/starmap_object.csv' DELIMITER ',' CSV HEADER"
+
+# SQL for generating CSVs from current data:
+# COPY public.grid TO '/tmp/grid.csv' DELIMITER ',' CSV HEADER;
+# COPY public.starmap_bg TO '/tmp/starmap_bg.csv' DELIMITER ',' CSV HEADER;
+# COPY public.starmap_object TO '/tmp/starmap_object.csv' DELIMITER ',' CSV HEADER;
