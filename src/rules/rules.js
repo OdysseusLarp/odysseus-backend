ec019f6c2c1e8e63d2a0909ad54a716869200f82
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-05-08 00:02:23 +0300

    chore: status logs for rule loading

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { isFunction } from 'lodash';
+import { logger } from '../logger';
 
 /* Require all *.js files in all subdirectories */
 
@@ -9,7 +10,9 @@ fs.readdirSync(__dirname, { withFileTypes: true }).forEach((entry) => {
 		const dir = path.join(__dirname, entry.name);
 		fs.readdirSync(dir).forEach((file) => {
 			if (file.endsWith('.js')) {
-				require(path.join(dir, file));
+				const filePath = path.join(dir, file);
+				require(filePath);
+				logger.info('Loaded rule file', filePath);
 			}
 		});
 	}
