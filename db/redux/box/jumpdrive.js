e61e0a3235a4c50bcda54e757ae2f8dfe2fb9721
Sampo Juustila <sampo.juustila@iki.fi>
2019-05-11 18:10:52 +0300

    feat: rename task to jump_drive_spectral_calibration

--- db/redux/box/jumpdrive.js
+++ db/redux/box/jumpdrive.js
@@ -1,17 +1,17 @@
 const blobs = [];
 
 blobs.push({
-	id: 'jump_drive_colors',
+	id: 'jump_drive_spectral_calibration',
 	type: 'box',
 	status: 'initial',
-	task: 'jump_drive_colors',
+	task: 'jump_drive_spectral_calibration',
 	context: {
 		color: 'red'  // FIXME: Update to proper context
 	}
 });
 
 blobs.push({
-	id: 'jump_drive_colors',
+	id: 'jump_drive_spectral_calibration',
 	type: 'task',
 	status: 'initial',
 	title: 'Jump drive configuration',
