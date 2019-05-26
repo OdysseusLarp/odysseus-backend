e29d14907744bd08453200c828702c72d58b3632
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-05-26 18:25:03 +0300

    fix: person and person_entry table changes

--- db/migrations/20190526104729_person-updates.js
+++ db/migrations/20190526104729_person-updates.js
@@ -1,5 +1,7 @@
 exports.up = async knex => {
-	await knex.raw(`ALTER TABLE ship ALTER COLUMN card_id DROP NOT NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN card_id DROP NOT NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN birth_year DROP NOT NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN last_name DROP NOT NULL`);
 
 	await knex.schema.alterTable('person', t => {
 		t.boolean('is_character');
@@ -9,7 +11,6 @@ exports.up = async knex => {
 		t.string('citizenship');
 		t.string('social_class');
 		t.string('political_party');
-		t.string('occupation');
 		t.string('military_rank');
 		t.text('military_remarks');
 		t.text('military_service_history');
@@ -17,7 +18,7 @@ exports.up = async knex => {
 		t.integer('medical_last_fitness_check');
 		t.string('medical_blood_type');
 		t.text('medical_allergies');
-		t.text('medical_activie_conditions');
+		t.text('medical_active_conditions');
 		t.text('medical_current_medication');
 		t.integer('created_year'); // year when the person was inserted into the system
 		t.dropColumn('dynasty_rank');
@@ -29,26 +30,32 @@ exports.up = async knex => {
 
 	// Table for medical/military/personal entries
 	await knex.schema.createTable('person_entry', t => {
+		t.increments('id').primary();
 		t.string('person_id').references('id').inTable('person').notNullable();
+		t.string('added_by').references('id').inTable('person');
 		t.string('type'); // MEDICAL, MILITARY, PERSONAL
 		t.text('entry');
 		t.timestamps(true, true);
-		t.primary('person_id');
+		t.index(['person_id']);
+		t.index(['type']);
 	});
 };
 
 exports.down = async knex => {
-	await knex.raw(`ALTER TABLE ship ALTER COLUMN updated_at SET NOT NULL`);
+	await knex.raw(`UPDATE person SET birth_year = 0 WHERE birth_year IS NULL`);
+	await knex.raw(`UPDATE person SET last_name = '<None>' WHERE last_name IS NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN updated_at SET NOT NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN birth_year SET NOT NULL`);
+	await knex.raw(`ALTER TABLE person ALTER COLUMN last_name SET NOT NULL`);
 
 	await knex.schema.alterTable('person', t => {
-		t.dropColumn('dynasty_rank');
+		t.string('dynasty_rank');
 		t.dropColumn('is_character');
 		t.dropColumn('citizen_id');
 		t.dropColumn('religion');
 		t.dropColumn('citizenship');
 		t.dropColumn('social_class');
 		t.dropColumn('political_party');
-		t.dropColumn('occupation');
 		t.dropColumn('military_rank');
 		t.dropColumn('military_remarks');
 		t.dropColumn('military_service_history');
@@ -56,7 +63,7 @@ exports.down = async knex => {
 		t.dropColumn('medical_last_fitness_check');
 		t.dropColumn('medical_blood_type');
 		t.dropColumn('medical_allergies');
-		t.dropColumn('medical_activie_conditions');
+		t.dropColumn('medical_active_conditions');
 		t.dropColumn('medical_current_medication');
 		t.dropColumn('created_year');
 	});
