3076eda1568a380e67927ff8845097b2035af8d1
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-05-28 22:30:28 +0300

    chore: define required node version, print error if rule loading fails because of node version

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -1,11 +1,16 @@
 import path from 'path';
 import fs from 'fs';
-import { isFunction } from 'lodash';
+import { isFunction, isString } from 'lodash';
 import { logger } from '../logger';
 
 /* Require all *.js files in all subdirectories */
 
 fs.readdirSync(__dirname, { withFileTypes: true }).forEach((entry) => {
+	if (isString(entry)) {
+		logger.warn(
+			`Rule file ${entry} recognized as a string instead of Dirent, check that you are running Node v10.10.0 or later`
+		);
+	}
 	if (isFunction(entry.isDirectory) && entry.isDirectory()) {
 		const dir = path.join(__dirname, entry.name);
 		fs.readdirSync(dir).forEach((file) => {
