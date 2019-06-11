0fb61f9e249b04cba0affe6f95786d45fa73ba7c
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-11 11:00:30 +0300

    feat: implement DMX output

--- src/dmx.js
+++ src/dmx.js
@@ -1,28 +1,60 @@
 import { logger } from './logger';
+import DMX from 'dmx';
 
-// const UNIVERSE = 2;
+
+const UNIVERSE_NAME = 'backend';
+const EVENT_DURATION = 1000;  // ms
+
 export const CHANNELS = {
-	JumpFixed: 0,
-	JumpPrepReady: 1,
-	JumpPrepStart: 2,
-	JumpApproved: 3,
-	JumpRejected: 4,
-	JumpPrepEnd: 5,
-	JumpReady: 6,
-	JumpInit: 7,
-	JumpStart: 8,
-	JumpBreaking: 9,
-	JumpEnd: 10,
-	JumpEndBreaking: 11,
-	JumpAbort: 12,
+	JumpFixed: 100,
+	JumpPrepReady: 101,
+	JumpPrepStart: 102,
+	JumpApproved: 103,
+	JumpRejected: 104,
+	JumpPrepEnd: 105,
+	JumpReady: 106,
+	JumpInit: 107,
+	JumpStart: 108,
+	JumpBreaking: 109,
+	JumpEnd: 110,
+	JumpEndBreaking: 111,
+	JumpAbort: 112,
+
+	BridgePowerSocketOn: 120,
+	BridgePowerSocketOff: 121,
+	EngineeringPowerSocketOn: 122,
+	EngineeringPowerSocketOff: 123,
+	MedbayPowerSocketOn: 124,
+	MedbayPowerSocketOff: 125,
+	SciencePowerSocketOn: 126,
+	SciencePowerSocketOff: 127,
+	LoungePowerSocketOn: 128,
+	LoungePowerSocketOff: 129,
 };
+
+const dmx = init();
+
+function init() {
+	if (process.env.DMX_DRIVER) {
+		const dmx = new DMX();
+		dmx.addUniverse(UNIVERSE_NAME, process.env.DMX_DRIVER);
+		return dmx;
+	} else {
+		return {
+			update() {}
+		};
+	}
+}
 
 export function setValue(channel, value) {
 	logger.debug(`Setting DMX channel ${channel} (${findChannelName(channel)}) to ${value}`);
+	dmx.update(UNIVERSE_NAME, { channel: value });
 }
 
 export function fireEvent(channel) {
 	logger.debug(`Firing event on DMX channel ${channel} (${findChannelName(channel)})`);
+	dmx.update(UNIVERSE_NAME, { channel: 255 });
+	setTimeout(() => dmx.update(UNIVERSE_NAME, { channel: 0 }), EVENT_DURATION);
 }
 
 function findChannelName(channel) {
