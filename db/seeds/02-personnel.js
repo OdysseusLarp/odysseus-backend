e9cda9add665f2349cb2583f44af544b30280ccb
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-06-20 17:00:40 +0300

    chore: add actual sip contacts

--- db/seeds/02-personnel.js
+++ db/seeds/02-personnel.js
@@ -30,8 +30,29 @@ exports.seed = async knex => {
 	await knex('person_family').insert(characterRelations);
 	await knex('person_entry').insert(characterEntries);
 	await knex('person_group').insert(characterGroups);
-	await knex('sip_contact').insert([
-		{ id: '6001', name: 'Contact #6001', video_allowed: false },
-		{ id: '6002', name: 'Contact #6002', video_allowed: false },
-	]);
+
+	const sipContacts = [
+		{ name: 'Bridge Comms 1', id: '6001', video_allowed: true },
+		{ name: 'Bridge Comms 2', id: '6002', video_allowed: true },
+		{ name: 'Engineering', id: '6003', video_allowed: false },
+		{ name: 'Crew bar', id: '6004', video_allowed: false },
+		{ name: 'Medical', id: '6005', video_allowed: false },
+		{ name: 'Science lab', id: '6006', video_allowed: false },
+		{ name: 'Captains room', id: '6007', video_allowed: true },
+		{ name: 'War room', id: '6008', video_allowed: true },
+		{ name: 'Security', id: '6009', video_allowed: false },
+		{ name: 'Hangar bay', id: '6010', video_allowed: false },
+		{ name: 'Fighter 1', id: '6011', video_allowed: false },
+		{ name: 'Fighter 2', id: '6012', video_allowed: false },
+		{ name: 'Fighter 3', id: '6013', video_allowed: false },
+		{ name: 'Fighter 4', id: '6014', video_allowed: false },
+		{ name: 'Fighter 5', id: '6015', video_allowed: false },
+		{ name: 'Fighter 6', id: '6016', video_allowed: false },
+		{ name: 'Big Screen', id: '6017', video_allowed: true },
+		{ name: 'Land team', id: '6018', video_allowed: true },
+		{ name: 'Fleet Secretary 1 ', id: '6019', video_allowed: true },
+		{ name: 'Fleet Secretary 2 ', id: '6020', video_allowed: true },
+		{ name: 'Fleet Secretary 3 ', id: '6021', video_allowed: true },
+	];
+	await knex('sip_contact').insert(sipContacts);
 };
