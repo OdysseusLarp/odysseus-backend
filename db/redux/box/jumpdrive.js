857292c91c5f900a9e460833011dac65866ae3ad
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-20 11:30:05 +0300

    feat: remove jump drive crystal installation + spectral color

--- db/redux/box/jumpdrive.js
+++ db/redux/box/jumpdrive.js
@@ -5,38 +5,14 @@ blobs.push({
 	type: 'box',
 	status: 'initial',
 	task: 'jump_drive_spectral_calibration',
-	context: {
-		color: 'red'  // FIXME: Update to proper context
-	}
 });
 
 blobs.push({
 	id: 'jump_drive_spectral_calibration',
 	type: 'task',
 	status: 'initial',
-	title: 'Jump drive configuration',
-	description: '...',
-	description_template: 'Jump drive configuration required.\n\nRequired state: {{color}}',
-	calibrationTime: 60,
-	calibrationCount: 1,
-	map: 'map01.png',
-	mapX: 100,
-	mapY: 200,
-});
-
-blobs.push({
-	id: 'jump_drive_crystal',
-	type: 'box',
-	status: 'initial',
-	task: 'jump_drive_crystal',
-});
-
-blobs.push({
-	id: 'jump_drive_crystal',
-	type: 'task',
-	status: 'initial',
-	title: 'Jump drive crystal replacement',
-	description: 'Install a new crystal into the jump drive.',
+	title: 'Jump drive spectral calibration',
+	description: '...',  // FIXME: Add description
 	calibrationTime: 60,
 	calibrationCount: 1,
 	map: 'map01.png',
