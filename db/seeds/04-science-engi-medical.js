c598cd50d2f7441129ca4231f63d77e0c6a195ee
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-06-27 01:24:12 +0300

    chore: add artifact csv parser and real initial data, remove dummy tag seeds

--- db/seeds/04-science-engi-medical.js
+++ db/seeds/04-science-engi-medical.js
@@ -1,81 +1,37 @@
-const artifacts = [
-	{
-		id: 1,
-		name: 'Shadowmourne',
-		catalog_id: 'ABC123',
-		discovered_by: 'Some guy',
-		discovered_at: 'Some planet',
-		discovered_from: 'What is this field even',
-		type: 'Alien artifact',
-		text: 'Some sort of an axe'
-	},
-	{
-		id: 2,
-		name: 'Rubber Duck',
-		catalog_id: 'DEF456',
-		discovered_by: 'Some other guy',
-		discovered_at: 'Some other planet',
-		discovered_from: 'What is this field even still',
-		type: 'Alien artifact',
-		text: 'Yellow duck'
-	}
-];
+const csv = require('csvtojson');
+const path = require('path');
+const { isEmpty } = require('lodash');
 
-const artifactEntry = [
-	{
-		artifact_id: 1,
-		person_id: '20000',
-		entry: `Scary looking axe that requires further analysis.`,
-	},
-	{
-		artifact_id: 1,
-		person_id: '20000',
-		entry: `Looks like it was once used by a gladiator in another universe.`,
-	},
-	{
-		artifact_id: 2,
-		person_id: '20000',
-		entry: `Duck says quack.`,
-	},
-	{
-		artifact_id: 2,
-		entry: `DNA Test indicates that the duck is indeed made of rubber.`,
-		person_id: '20000',
-	},
-	{
-		artifact_id: 2,
-		entry: `X-Ray pictures show that there is something inside the duck.`,
-		person_id: '20001',
-	}
-];
+async function getArtifacts() {
+	const csvPath = path.join(__dirname, '../data/artifacts.csv');
+	const rawArtifacts = await csv().fromFile(csvPath);
 
-const tags = [
-	{
-		id: 'TAG001',
-		type: 'DIAGNOSIS',
-		description: 'Seems like this hand is broken.',
-		metadata: null
-	},
-	{
-		id: 'TAG002',
-		type: 'DIAGNOSIS',
-		description: 'Smells like blood poisoning.',
-		metadata: null
-	},
-	{
-		id: 'TAG003',
-		type: 'ENGINEERING', // Some generic engineering tag
-		description: 'Not sure about this',
-		metadata: null
-	}
-];
+	const artifactEntries = [];
+	const artifacts = rawArtifacts.map((artifact, i) => {
+		const id = i + 1;
+		artifact.id = id;
+		['catalog_id', 'name', 'discovered_at', 'discovered_by', 'discovered_from', 'type', 'text'].forEach(col => {
+			if (!artifact[col]) delete artifact[col];
+		});
+		if ('entry' in artifact && !isEmpty(artifact.entry)) {
+			artifactEntries.push({
+				artifact_id: id,
+				entry: artifact.entry
+			});
+		}
+		delete artifact.entry;
+		return artifact;
+	});
+	return { artifacts, artifactEntries };
+}
 
 exports.seed = async knex => {
+	const { artifacts, artifactEntries } = await getArtifacts();
 	await knex('artifact_entry').del();
 	await knex('artifact').del();
 	await knex('tag').del();
-	await knex('tag').insert(tags);
+	// await knex('tag').insert(tags);
 	await knex('artifact').insert(artifacts);
-	await knex('artifact_entry').insert(artifactEntry);
-	await knex.raw(`SELECT setval('artifact_id_seq', 3)`);
+	await knex('artifact_entry').insert(artifactEntries);
+	await knex.raw(`SELECT setval('artifact_id_seq', ${artifacts.length + 1})`);
 };
