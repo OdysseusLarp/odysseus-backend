94b818c87106fb3fac4ee2a0174f04a53d79847d
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-27 16:15:00 +0300

    feat: implement velian game seeds

--- db/redux/game/index.js
+++ db/redux/game/index.js
@@ -1,6 +1,8 @@
 import { saveBlob } from '../helpers';
 import eeHealth from './eeHealthTasks';
 import manualTasks from './manualTasks';
+import velian from './velian';
 
 eeHealth.forEach(saveBlob);
 manualTasks.forEach(saveBlob);
+velian.forEach(saveBlob);
---
+++ db/redux/game/velian.js
@@ -0,0 +1,72 @@
+const blobs = [];
+
+blobs.push({
+	type: 'game',
+	id: 'velian1',
+	game_config: 'velian1',
+	status: 'broken',
+});
+
+blobs.push({
+	type: 'game_config',
+	id: 'velian1',
+
+	default: {
+		initDescription: '...',
+		endDescription: '...!',
+		game: 'phasesync',
+
+		dimensions: 2,
+		difficulty: 0.1,
+		duration: 5*60,
+		drift: 0.04,
+	},
+});
+
+
+blobs.push({
+	type: 'game',
+	id: 'velian2',
+	game_config: 'velian2',
+	status: 'broken',
+});
+
+blobs.push({
+	type: 'game_config',
+	id: 'velian2',
+
+	default: {
+		initDescription: '...',
+		endDescription: '...!',
+		game: 'phasesync',
+
+		dimensions: 5,
+		difficulty: 0.5,
+		duration: 1,
+		drift: 0,
+	},
+});
+
+
+blobs.push({
+	type: 'game',
+	id: 'velian3',
+	game_config: 'velian3',
+	status: 'broken',
+});
+
+blobs.push({
+	type: 'game_config',
+	id: 'velian3',
+
+	default: {
+		initDescription: '...',
+		endDescription: '...!',
+		game: 'lightsout',
+		random: 30,
+		size: 5,
+	},
+});
+
+
+export default blobs;
