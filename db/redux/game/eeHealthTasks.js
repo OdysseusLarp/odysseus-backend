77b4676dbaffa968450f565c70c57bbee9048099
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-27 11:24:03 +0300

    feat: implement ee health task configs

--- db/redux/game/eeHealthTasks.js
+++ db/redux/game/eeHealthTasks.js
@@ -1,7 +1,7 @@
 const blobs = [];
 
 
-// Reactor
+// Reactor  DONE
 
 for (let i=0; i < 20; i++) {
 	const letter = String.fromCharCode('A'.charCodeAt(0) + Math.floor(i/5));
@@ -29,7 +29,7 @@ for (let i=0; i < 20; i++) {
 		calibrationTime: 0,
 		calibrationCount: 0,
 		title: `Reactor power segment ${code}`,
-		description: `Reactor power segment ${code} has failed.`, // FIXME: babbletize
+		description: `Reactor power segment ${code} has failed. Phase shift needs to be recalibrated using HANSCA.`,
 		location: 'Upper deck, security room',
 		map: 'upper-7.png',
 		mapX: 240,
@@ -39,28 +39,46 @@ for (let i=0; i < 20; i++) {
 
 
 blobs.push({
-	type: 'game_config',
-	id: 'reactor',
-	default: {
-		initDescription: 'Reactor power segment needs phase calibration',  // FIXME: babbletize
+	'type': 'game_config',
+	'id': 'reactor',
+
+	'default': {
+		initDescription: 'Reactor power segment needs phase calibration.',
 		endDescription: 'Phase calibration successful!',
 		game: 'phasesync',
 
-		// FIXME: Fine-tune settings for suitable difficulty
+		dimensions: 2,
+		difficulty: 0.4,
+		duration: 1,
+		drift: 0.15,
+	},
+
+	'skill:master': {
+		initDescription: 'Reactor power segment needs phase calibration.',
+		endDescription: 'Phase calibration successful!',
+		game: 'phasesync',
+
+		dimensions: 2,
+		difficulty: 0.4,
+		duration: 1,
+		drift: 0.05,
+	},
+
+	'skill:expert': {
+		initDescription: 'Reactor power segment needs phase calibration.',
+		endDescription: 'Phase calibration successful!',
+		game: 'phasesync',
+
 		dimensions: 2,
 		difficulty: 0.2,
 		duration: 1,
 		drift: 0,
 	},
-	// engineer_proficient: {
-	// 	game: 'phasesync'
-	// 	...
-	// }
 });
 
 
 
-// Engine
+// Engine  DONE
 
 for (let i=0; i < 20; i++) {
 	const letter = String.fromCharCode('A'.charCodeAt(0) + Math.floor(i/5));
@@ -75,7 +93,7 @@ for (let i=0; i < 20; i++) {
 		game_config: 'impulse',
 		status: 'fixed',
 		config: {
-			title: `Engine power segment ${code}`,
+			title: `Impulse engine power segment ${code}`,
 		},
 	});
 	blobs.push({
@@ -87,34 +105,52 @@ for (let i=0; i < 20; i++) {
 		status: 'initial',
 		calibrationTime: 0,
 		calibrationCount: 0,
-		title: `Engine power segment ${code}`,
-		description: `Engine power segment ${code} has failed.`, // FIXME: babbletize
-		location: 'Lower deck, mess hall', // FIXME: location + map, baaritiski
-		map: 'upper-7.png',
-		mapX: 240,
-		mapY: 310 + i*2,
+		title: `Impulse engine power segment ${code}`,
+		description: `Impulse engine power segment ${code} has failed. Phase shift needs to be recalibrated using HANSCA.`,
+		location: 'Lower deck, mess hall',
+		map: 'lower-11.png',
+		mapX: 130,
+		mapY: 220,
 	});
 }
 
 
 blobs.push({
-	type: 'game_config',
-	id: 'impulse',
-	default: {
-		initDescription: 'Engine power segment needs phase calibration',  // FIXME: babbletize
+	'type': 'game_config',
+	'id': 'impulse',
+
+	'default': {
+		initDescription: 'Impulse engine power segment needs phase calibration.',
 		endDescription: 'Phase calibration successful!',
 		game: 'phasesync',
 
-		// FIXME: Fine-tune settings for suitable difficulty
-		dimensions: 2,
-		difficulty: 0.2,
-		duration: 1,
-		drift: 0,
+		dimensions: 1,
+		difficulty: 0,
+		duration: 30,
+		drift: 0.3,
 	},
-	// engineer_proficient: {
-	// 	game: 'phasesync'
-	// 	...
-	// }
+
+	'skill:master': {
+		initDescription: 'Impulse engine power segment needs phase calibration.',
+		endDescription: 'Phase calibration successful!',
+		game: 'phasesync',
+
+		dimensions: 1,
+		difficulty: 0,
+		duration: 30,
+		drift: 0.15,
+	},
+
+	'skill:expert': {
+		initDescription: 'Impulse engine power segment needs phase calibration.',
+		endDescription: 'Phase calibration successful!',
+		game: 'phasesync',
+
+		dimensions: 1,
+		difficulty: 0,
+		duration: 20,
+		drift: 0.1,
+	},
 });
 
 
@@ -300,7 +336,7 @@ blobs.push({
 
 
 
-// Missile system
+// Missile system  DONE
 
 for (let i=0; i < 20; i++) {
 	const number = i + 1;
@@ -314,7 +350,7 @@ for (let i=0; i < 20; i++) {
 		game_config: 'missilesystem',
 		status: 'fixed',
 		config: {
-			title: `Missile system power segment ${code}`,
+			title: `Missile tube region ${code} damaged`,
 		},
 	});
 	blobs.push({
@@ -326,34 +362,56 @@ for (let i=0; i < 20; i++) {
 		status: 'initial',
 		calibrationTime: 0,
 		calibrationCount: 0,
-		title: `Missile system power segment ${code}`,
-		description: `Missile system power segment ${code} has failed.`, // FIXME: babbletize
-		location: 'Upper deck, security room', // FIXME: location + map
-		map: 'upper-7.png',
-		mapX: 240,
-		mapY: 310 + i*2,
+		title: `Missile tube region ${code} damaged`,
+		description: `Missile tube region ${code} is damaged. Drone needs to be sent for repairs. Use HANSCA for performing repairs.`,
+		location: 'Lower deck, under stairway',
+		map: 'lower-13.png',
+		mapX: 430,
+		mapY: 200,
 	});
 }
 
 
 blobs.push({
-	type: 'game_config',
-	id: 'missilesystem',
-	default: {
-		initDescription: 'Missile system power segment needs phase calibration',  // FIXME: babbletize
-		endDescription: 'Phase calibration successful!',
-		game: 'phasesync',
+	'type': 'game_config',
+	'id': 'missilesystem',
 
-		// FIXME: Fine-tune settings for suitable difficulty
-		dimensions: 2,
-		difficulty: 0.2,
-		duration: 1,
-		drift: 0,
+	'default': {
+		initDescription: `<p>Missile system repairs require sending a drone.</p>
+		<p>You must fly the drone to the missile tube. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at missile tube. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.7,
+		score: 20
 	},
-	// engineer_proficient: {
-	// 	game: 'phasesync'
-	// 	...
-	// }
+
+	'skill:master': {
+		initDescription: `<p>Missile system repairs require sending a drone.</p>
+		<p>You must fly the drone to the missile tube. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at missile tube. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.6,
+		score: 10
+	},
+
+	'skill:expert': {
+		initDescription: `<p>Missile system repairs require sending a drone.</p>
+		<p>You must fly the drone to the missile tube. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at missile tube. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.3,
+		score: 7
+	},
+
 });
 
 
@@ -417,7 +475,7 @@ blobs.push({
 
 
 
-// Hull
+// Hull  DONE
 
 for (let i=0; i < 20; i++) {
 	let letter;
@@ -437,7 +495,7 @@ for (let i=0; i < 20; i++) {
 		game_config: 'hull',
 		status: 'fixed',
 		config: {
-			title: `Hull power segment ${code}`,
+			title: `Hull region ${code} damaged`,
 		},
 	});
 	blobs.push({
@@ -449,34 +507,56 @@ for (let i=0; i < 20; i++) {
 		status: 'initial',
 		calibrationTime: 0,
 		calibrationCount: 0,
-		title: `Hull power segment ${code}`,
-		description: `Hull power segment ${code} has failed.`, // FIXME: babbletize
-		location: 'Lower deck, stairways', // FIXME: location + map portaat
-		map: 'upper-7.png',
-		mapX: 240,
-		mapY: 310 + i*2,
+		title: `Hull region ${code} damaged`,
+		description: `Hull region ${code} is damaged. Drone needs to be sent for repairs. Use HANSCA for performing repairs.`,
+		location: 'Lower deck, stairway',
+		map: letter === 'L' ? 'lower-6.png' : 'lower-13.png',
+		mapX: letter === 'L' ? 220: 430,
+		mapY: letter === 'L' ? 250 : 230,
 	});
 }
 
 
 blobs.push({
-	type: 'game_config',
-	id: 'hull',
-	default: {
-		initDescription: 'Hull power segment needs phase calibration',  // FIXME: babbletize
-		endDescription: 'Phase calibration successful!',
-		game: 'phasesync',
+	'type': 'game_config',
+	'id': 'hull',
 
-		// FIXME: Fine-tune settings for suitable difficulty
-		dimensions: 2,
-		difficulty: 0.2,
-		duration: 1,
-		drift: 0,
+	'default': {
+		initDescription: `<p>Manual hull repairs required using a drone.</p>
+		<p>You must fly the drone to the hull area. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at hull. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.7,
+		score: 20
 	},
-	// engineer_proficient: {
-	// 	game: 'phasesync'
-	// 	...
-	// }
+
+	'skill:master': {
+		initDescription: `<p>Manual hull repairs required using a drone.</p>
+		<p>You must fly the drone to the hull area. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at hull. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.6,
+		score: 10
+	},
+
+	'skill:expert': {
+		initDescription: `<p>Manual hull repairs required using a drone.</p>
+		<p>You must fly the drone to the hull area. Tap HANSCA to fly upwards.</p>`,
+		endDescription: 'Drone successfully arrived at hull. Repairs started.',
+		failDescription: 'Drone destroyed!',
+		game: 'flappy',
+		gap: 150,
+		interval: 250,
+		gravity: 1.3,
+		score: 7
+	},
+
 });
 
 
