bc86ec39c2c7430269da268ec77dbb73badac2f1
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-07-04 00:47:58 +0300

    feat: add tags loader and initial tag seeds

--- db/seeds/04-science-engi-medical.js
+++ db/seeds/04-science-engi-medical.js
@@ -1,6 +1,6 @@
 const csv = require('csvtojson');
 const path = require('path');
-const { isEmpty } = require('lodash');
+const { isEmpty, get } = require('lodash');
 
 async function getArtifacts() {
 	const csvPath = path.join(__dirname, '../data/artifacts.csv');
@@ -25,12 +25,24 @@ async function getArtifacts() {
 	return { artifacts, artifactEntries };
 }
 
+async function getTags() {
+	const csvPath = path.join(__dirname, '../data/tags.csv');
+	const rawTags = await csv().fromFile(csvPath);
+
+	const tags = rawTags.map(tag => ({
+		id: get(tag, 'tag_id', '').trim(),
+		type: get(tag, 'type', '').toUpperCase().trim(),
+		description: get(tag, 'description', '').trim(),
+	})).filter(tag => tag.id && tag.type && tag.description);
+	return tags;
+}
+
 exports.seed = async knex => {
 	const { artifacts, artifactEntries } = await getArtifacts();
 	await knex('artifact_entry').del();
 	await knex('artifact').del();
 	await knex('tag').del();
-	// await knex('tag').insert(tags);
+	await knex('tag').insert(await getTags());
 	await knex('artifact').insert(artifacts);
 	await knex('artifact_entry').insert(artifactEntries);
 	await knex.raw(`SELECT setval('artifact_id_seq', ${artifacts.length + 1})`);
