7aa58b4cb8b3c922079a392c434b2ffd9ce2a783
Sampo Juustila <sampo.juustila@iki.fi>
2019-07-07 17:18:11 +0300

    feat: add gas and radiation leak tasks to seed

--- db/redux/game/index.js
+++ db/redux/game/index.js
@@ -3,8 +3,10 @@ import eeHealth from './eeHealthTasks';
 import manualTasks from './manualTasks';
 import velian from './velian';
 import lifesupport from './lifesupport';
+import eventTasks from './eventTasks';
 
 eeHealth.forEach(saveBlob);
 manualTasks.forEach(saveBlob);
 velian.forEach(saveBlob);
 lifesupport.forEach(saveBlob);
+eventTasks.forEach(saveBlob);
