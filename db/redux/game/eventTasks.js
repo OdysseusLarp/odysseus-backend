36958284e183b0b52e7d60f6e71d22588a76f03e
Sampo Juustila <sampo.juustila@iki.fi>
2019-07-23 09:50:27 +0300

    feat: add demo game

--- db/redux/game/eventTasks.js
+++ db/redux/game/eventTasks.js
@@ -1,4 +1,27 @@
 const blobs = [
+	{
+		type: 'game',
+		id: 'demo',
+		game_config: 'demo',
+		status: 'broken',
+		config: {
+			title: 'Demo task'
+		}
+	},
+	{
+		type: 'game_config',
+		id: 'demo',
+		default: {
+			initDescription: 'Demo needs phase calibration.',
+			endDescription: 'Phase calibration successful!',
+			game: 'phasesync',
+
+			dimensions: 2,
+			difficulty: 0,
+			duration: 3,
+			drift: 0,
+		},
+	},
 ];
 
 export default blobs;
