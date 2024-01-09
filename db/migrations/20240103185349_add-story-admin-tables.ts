5dad6c9ff41254d0a119cae38799b04a048cf773
Nico Hagelberg <nicou@nicou.me>
2024-01-09 21:21:11 +0200

    Add db migrations for remaining story admin tables

--- db/migrations/20240103185349_add-story-admin-tables.ts
+++ db/migrations/20240103185349_add-story-admin-tables.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable('events', (table) => {
+	await knex.schema.createTable('story_events', (table) => {
 		table.increments('id').primary();
 		table.text('name').notNullable();
 		table.text('character_groups').nullable();
@@ -25,7 +25,7 @@ export async function up(knex: Knex): Promise<void> {
 	});
 
 	// joining table for characters to events
-	await knex.schema.createTable('person_events', (table) => {
+	await knex.schema.createTable('story_person_events', (table) => {
 		table
 			.string('person_id')
 			.notNullable()
@@ -36,13 +36,13 @@ export async function up(knex: Knex): Promise<void> {
 			.integer('event_id')
 			.notNullable()
 			.references('id')
-			.inTable('events')
+			.inTable('story_events')
 			.onDelete('CASCADE');
 		table.primary(['person_id', 'event_id']);
 	});
 
 	// joining table for artifacts to events
-	await knex.schema.createTable('artifact_events', (table) => {
+	await knex.schema.createTable('story_artifact_events', (table) => {
 		table
 			.integer('artifact_id')
 			.notNullable()
@@ -53,12 +53,12 @@ export async function up(knex: Knex): Promise<void> {
 			.integer('event_id')
 			.notNullable()
 			.references('id')
-			.inTable('events')
+			.inTable('story_events')
 			.onDelete('CASCADE');
 		table.primary(['artifact_id', 'event_id']);
 	});
 
-	await knex.schema.createTable('plots', (table) => {
+	await knex.schema.createTable('story_plots', (table) => {
 		table.integer('id').primary(); // Assuming 'id' is the primary key
 		table.text('name').notNullable();
 		table.text('character_groups').nullable(); // comma separated list of character group ids
@@ -81,7 +81,7 @@ export async function up(knex: Knex): Promise<void> {
 	});
 
 	// character_ids need to be linked to person_plots
-	await knex.schema.createTable('person_plots', (table) => {
+	await knex.schema.createTable('story_person_plots', (table) => {
 		table
 			.string('person_id')
 			.notNullable()
@@ -92,13 +92,13 @@ export async function up(knex: Knex): Promise<void> {
 			.integer('plot_id')
 			.notNullable()
 			.references('id')
-			.inTable('plots')
+			.inTable('story_plots')
 			.onDelete('CASCADE');
 		table.primary(['person_id', 'plot_id']);
 	});
 
 	// artifact_ids need to be linked to person_plots
-	await knex.schema.createTable('artifact_plots', (table) => {
+	await knex.schema.createTable('story_artifact_plots', (table) => {
 		table
 			.integer('artifact_id')
 			.notNullable()
@@ -109,36 +109,36 @@ export async function up(knex: Knex): Promise<void> {
 			.integer('plot_id')
 			.notNullable()
 			.references('id')
-			.inTable('plots')
+			.inTable('story_plots')
 			.onDelete('CASCADE');
 		table.primary(['artifact_id', 'plot_id']);
 	});
 
 	// create a joining table for events to plots
-	await knex.schema.createTable('event_plots', (table) => {
+	await knex.schema.createTable('story_event_plots', (table) => {
 		table
 			.integer('event_id')
 			.notNullable()
 			.references('id')
-			.inTable('events')
+			.inTable('story_events')
 			.onDelete('CASCADE');
 		table
 			.integer('plot_id')
 			.notNullable()
 			.references('id')
-			.inTable('plots')
+			.inTable('story_plots')
 			.onDelete('CASCADE');
 		table.primary(['event_id', 'plot_id']);
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('event_plots');
-  await knex.schema.dropTableIfExists('artifact_plots');
-  await knex.schema.dropTableIfExists('person_plots');
-  await knex.schema.dropTableIfExists('artifact_events');
-  await knex.schema.dropTableIfExists('person_events');
+	await knex.schema.dropTableIfExists('story_event_plots');
+	await knex.schema.dropTableIfExists('story_artifact_plots');
+	await knex.schema.dropTableIfExists('story_person_plots');
+	await knex.schema.dropTableIfExists('story_artifact_events');
+	await knex.schema.dropTableIfExists('story_person_events');
 
-  await knex.schema.dropTableIfExists('plots');
-  await knex.schema.dropTableIfExists('events');
+	await knex.schema.dropTableIfExists('story_plots');
+	await knex.schema.dropTableIfExists('story_events');
 }
---
+++ db/migrations/20240109184905_add-story-admin-messages.ts
@@ -0,0 +1,90 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('story_messages', (table) => {
+		table.increments('id').primary();
+		table.string('name').notNullable();
+		table
+			.string('sender_person_id')
+			.nullable()
+			.references('id')
+			.inTable('person')
+			.onDelete('CASCADE');
+		table
+			.string('receiver_person_id')
+			.nullable()
+			.references('id')
+			.inTable('person')
+			.onDelete('CASCADE');
+		// Types:
+		// Datahub message types: Text NPC, EVA, Hints for scientist, Fleet Coms, Fleet Secretary, Fleet Admiral
+		// Ship log messages
+		// News (Posted to Datahub)
+		table.string('type').notNullable();
+		// The plot takes place after this jump number
+		table.integer('after_jump').nullable();
+		// If set to true, the plot should happen exactly at the jump number in "after_jump"
+		table.boolean('locked').notNullable().defaultTo(false);
+		// Possible 'sent' values:
+		// Yes, No need, Not yet, Repeatable
+		table.string('sent').notNullable().defaultTo('Not yet');
+		table.text('message').notNullable();
+		table.text('gm_notes').nullable();
+	});
+
+	await knex.schema.createTable('story_person_messages', (table) => {
+		table
+			.string('person_id')
+			.notNullable()
+			.references('id')
+			.inTable('person')
+			.onDelete('CASCADE');
+		table
+			.integer('message_id')
+			.notNullable()
+			.references('id')
+			.inTable('story_messages')
+			.onDelete('CASCADE');
+		table.primary(['person_id', 'message_id']);
+	});
+
+	await knex.schema.createTable('story_plot_messages', (table) => {
+		table
+			.integer('plot_id')
+			.notNullable()
+			.references('id')
+			.inTable('story_plots')
+			.onDelete('CASCADE');
+		table
+			.integer('message_id')
+			.notNullable()
+			.references('id')
+			.inTable('story_messages')
+			.onDelete('CASCADE');
+		table.primary(['plot_id', 'message_id']);
+	});
+
+	await knex.schema.createTable('story_event_messages', (table) => {
+		table
+			.integer('event_id')
+			.notNullable()
+			.references('id')
+			.inTable('story_events')
+			.onDelete('CASCADE');
+		table
+			.integer('message_id')
+			.notNullable()
+			.references('id')
+			.inTable('story_messages')
+			.onDelete('CASCADE');
+		table.primary(['event_id', 'message_id']);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTableIfExists('story_event_messages');
+	await knex.schema.dropTableIfExists('story_plot_messages');
+	await knex.schema.dropTableIfExists('story_person_messages');
+
+	await knex.schema.dropTableIfExists('story_messages');
+}
