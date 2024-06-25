3c078cbd9e5b61cec5bf17e154f88b38126736a7
Sampo Juustila <sampo.juustila@iki.fi>
2024-06-25 11:01:08 +0300

    feat: update TP-link power socket config

--- db/redux/tplink/index.ts
+++ db/redux/tplink/index.ts
@@ -5,7 +5,23 @@ saveBlob({
 	type: 'tplink',
 	id: 'dmxconfig',
 	// List of objects { dmx: "LoungeFuseFixed", ip: "1.2.3.4", powerstate: true }
-	signals: [], // TODO: Populate
+	signals: [
+		// Medbay examination light broken by Medbay fuse
+		{ dmx: 'MedbayFuseBroken', ip: '172.30.4.70', powerstate: false },
+		{ dmx: 'MedbayFuseFixed', ip: '172.30.4.70', powerstate: true },
+		// Science lab research lights broken by Science fuse
+		{ dmx: 'ScienceFuseBroken', ip: '172.30.4.71', powerstate: false },
+		{ dmx: 'ScienceFuseFixed', ip: '172.30.4.71', powerstate: true },
+		// Security room security camera displays broken by Lounge fuse
+		{ dmx: 'LoungeFuseBroken', ip: '172.30.4.72', powerstate: false },
+		{ dmx: 'LoungeFuseFixed', ip: '172.30.4.72', powerstate: true },
+		// Engine room (right) displays broken by Engineering fuse
+		{ dmx: 'EngineeringFuseBroken', ip: '172.30.4.73', powerstate: false },
+		{ dmx: 'EngineeringFuseFixed', ip: '172.30.4.73', powerstate: true },
+		// Engine room (right) plasma ball broken by Thermic Fusion Regulator / kick box task
+		{ dmx: 'ThermicFusionRegulatorBroken', ip: '172.30.4.74', powerstate: false },
+		{ dmx: 'ThermicFusionRegulatorFixed', ip: '172.30.4.74', powerstate: true },
+	],
 });
 
 // Device information, scanned regularly by backend based on 'dmxconfig' blob.
