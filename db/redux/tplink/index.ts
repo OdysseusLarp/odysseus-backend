33542025f616d6a74e86c3cd0aa62c23836f41b8
Sampo Juustila <sampo.juustila@iki.fi>
2024-12-25 22:03:48 +0200

    chore: disable all TP-link scanning

--- db/redux/tplink/index.ts
+++ db/redux/tplink/index.ts
@@ -1,41 +1,41 @@
 import { saveBlob } from '../helpers';
 
 // DMX signals to turn sockets on or off using socket device IP.
-saveBlob({
-	type: 'tplink',
-	id: 'dmxconfig',
-	// List of objects { dmx: "LoungeFuseFixed", ip: "1.2.3.4", powerstate: true }
-	signals: [
-		// Medbay examination light broken by Medbay fuse
-		{ dmx: 'MedbayFuseBroken', ip: '172.30.4.70', powerstate: false },
-		{ dmx: 'MedbayFuseFixed', ip: '172.30.4.70', powerstate: true },
+// saveBlob({
+// 	type: 'tplink',
+// 	id: 'dmxconfig',
+// 	// List of objects { dmx: "LoungeFuseFixed", ip: "1.2.3.4", powerstate: true }
+// 	signals: [
+// 		// Medbay examination light broken by Medbay fuse
+// 		{ dmx: 'MedbayFuseBroken', ip: '172.30.4.70', powerstate: false },
+// 		{ dmx: 'MedbayFuseFixed', ip: '172.30.4.70', powerstate: true },
 
-		// Science lab research lights broken by Science fuse and jump
-		{ dmx: 'ScienceFuseBroken', ip: '172.30.4.71', powerstate: false },
-		{ dmx: 'ScienceFuseFixed', ip: '172.30.4.71', powerstate: true },
-		{ dmx: 'JumpStart', ip: '172.30.4.71', powerstate: false },
-		{ dmx: 'JumpEnd', ip: '172.30.4.71', powerstate: true },
-		{ dmx: 'JumpEndBreaking', ip: '172.30.4.71', powerstate: true },
+// 		// Science lab research lights broken by Science fuse and jump
+// 		{ dmx: 'ScienceFuseBroken', ip: '172.30.4.71', powerstate: false },
+// 		{ dmx: 'ScienceFuseFixed', ip: '172.30.4.71', powerstate: true },
+// 		{ dmx: 'JumpStart', ip: '172.30.4.71', powerstate: false },
+// 		{ dmx: 'JumpEnd', ip: '172.30.4.71', powerstate: true },
+// 		{ dmx: 'JumpEndBreaking', ip: '172.30.4.71', powerstate: true },
 
-		// Security room security camera displays broken by Lounge fuse and jump
-		{ dmx: 'LoungeFuseBroken', ip: '172.30.4.72', powerstate: false },
-		{ dmx: 'LoungeFuseFixed', ip: '172.30.4.72', powerstate: true },
-		{ dmx: 'JumpStart', ip: '172.30.4.72', powerstate: false },
-		{ dmx: 'JumpEnd', ip: '172.30.4.72', powerstate: true },
-		{ dmx: 'JumpEndBreaking', ip: '172.30.4.72', powerstate: true },
+// 		// Security room security camera displays broken by Lounge fuse and jump
+// 		{ dmx: 'LoungeFuseBroken', ip: '172.30.4.72', powerstate: false },
+// 		{ dmx: 'LoungeFuseFixed', ip: '172.30.4.72', powerstate: true },
+// 		{ dmx: 'JumpStart', ip: '172.30.4.72', powerstate: false },
+// 		{ dmx: 'JumpEnd', ip: '172.30.4.72', powerstate: true },
+// 		{ dmx: 'JumpEndBreaking', ip: '172.30.4.72', powerstate: true },
 
-		// Engine room (right) displays broken by Engineering fuse and jump
-		{ dmx: 'EngineeringFuseBroken', ip: '172.30.4.73', powerstate: false },
-		{ dmx: 'EngineeringFuseFixed', ip: '172.30.4.73', powerstate: true },
-		{ dmx: 'JumpStart', ip: '172.30.4.73', powerstate: false },
-		{ dmx: 'JumpEnd', ip: '172.30.4.73', powerstate: true },
-		{ dmx: 'JumpEndBreaking', ip: '172.30.4.73', powerstate: true },
+// 		// Engine room (right) displays broken by Engineering fuse and jump
+// 		{ dmx: 'EngineeringFuseBroken', ip: '172.30.4.73', powerstate: false },
+// 		{ dmx: 'EngineeringFuseFixed', ip: '172.30.4.73', powerstate: true },
+// 		{ dmx: 'JumpStart', ip: '172.30.4.73', powerstate: false },
+// 		{ dmx: 'JumpEnd', ip: '172.30.4.73', powerstate: true },
+// 		{ dmx: 'JumpEndBreaking', ip: '172.30.4.73', powerstate: true },
 
-		// Engine room (right) plasma ball broken by Thermic Fusion Regulator / kick box task
-		{ dmx: 'ThermicFusionRegulatorBroken', ip: '172.30.4.74', powerstate: false },
-		{ dmx: 'ThermicFusionRegulatorFixed', ip: '172.30.4.74', powerstate: true },
-	],
-});
+// 		// Engine room (right) plasma ball broken by Thermic Fusion Regulator / kick box task
+// 		{ dmx: 'ThermicFusionRegulatorBroken', ip: '172.30.4.74', powerstate: false },
+// 		{ dmx: 'ThermicFusionRegulatorFixed', ip: '172.30.4.74', powerstate: true },
+// 	],
+// });
 
 // Device information, scanned regularly by backend based on 'dmxconfig' blob.
 // Informational only, not used by backend.
