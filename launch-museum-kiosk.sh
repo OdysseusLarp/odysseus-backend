c4eb41f6ed98ef2321adfee321096d74bf154250
Sampo Juustila <sampo.juustila@iki.fi>
2024-12-25 22:52:07 +0200

    chore: set background image on startup

--- launch-museum-kiosk.sh
+++ launch-museum-kiosk.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if mount | grep -q overlay; then
   echo "Launching server + browser"
+  # Set background image to launch screen
+  xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorLVDS-1/workspace0/last-image -s "$SCRIPT_DIR/museum/background.jpg"
+
   ./launch-browser.sh
 else
   echo "Read-write mode in use, not starting. Use ./launch-server.sh and ./launch-browser.sh as necessary."
