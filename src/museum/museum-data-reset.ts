87169f8298a208c3c4924b9c967f3f273aec42b2
Sampo Juustila <sampo.juustila@iki.fi>
2025-01-22 22:16:40 +0200

    feat: voting reset

--- src/museum/museum-data-reset.ts
+++ src/museum/museum-data-reset.ts
@@ -3,6 +3,9 @@ import artifactDump from 'museum/data/artifact.json';
 import postDump from 'museum/data/post.json';
 import messageDump from 'museum/data/com_message.json';
 import shipLogDump from 'museum/data/ship_log.json';
+import voteDump from 'museum/data/vote.json';
+import voteOptionDump from 'museum/data/vote_option.json';
+import voteEntryDump from 'museum/data/vote_entry.json';
 
 /**
  * Update timestamps to the same difference from now as toBeUpdates is from referenceTime
@@ -42,4 +45,7 @@ export async function resetMuseumDatabase() {
 	await resetTable('post', postDump, 'created_at', '2024-07-05 01:37:00.000+03');
 	await resetTable('com_message', messageDump, 'created_at', '2025-01-22 20:00:00+02');
 	await resetTable('ship_log', shipLogDump, 'created_at', '2024-07-05 00:07:00.000+03');
+	await resetTable('vote', voteDump, 'active_until', '2024-07-05 14:00:00.000+03'); // Cascade deletes also vote_entry
+	await resetTable('vote_option', voteOptionDump);
+	await resetTable('vote_entry', voteEntryDump);
 }
