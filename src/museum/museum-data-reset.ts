3b297613d0631851155b1d5ad0e43b92cfa7451a
Sampo Juustila <sampo.juustila@iki.fi>
2025-01-24 22:04:52 +0200

    feat: more db reset

--- src/museum/museum-data-reset.ts
+++ src/museum/museum-data-reset.ts
@@ -1,11 +1,13 @@
 import { knex } from '../../db';
 import artifactDump from 'museum/data/artifact.json';
+import artifactEntryDump from 'museum/data/artifact_entry.json';
 import postDump from 'museum/data/post.json';
 import messageDump from 'museum/data/com_message.json';
 import shipLogDump from 'museum/data/ship_log.json';
 import voteDump from 'museum/data/vote.json';
 import voteOptionDump from 'museum/data/vote_option.json';
 import voteEntryDump from 'museum/data/vote_entry.json';
+import personEntryDump from 'museum/data/person_entry.json';
 
 /**
  * Update timestamps to the same difference from now as toBeUpdates is from referenceTime
@@ -37,15 +39,21 @@ async function resetTable(
 		updatedData = data;
 	}
 	await knex(tableName).del();
-	await knex(tableName).insert(updatedData);
+	if (updatedData.length > 0) {
+		await knex(tableName).insert(updatedData);
+	}
 }
 
 export async function resetMuseumDatabase() {
 	await resetTable('artifact', artifactDump);
+	await resetTable('artifact_entry', artifactEntryDump);
 	await resetTable('post', postDump, 'created_at', '2024-07-05 01:37:00.000+03');
 	await resetTable('com_message', messageDump, 'created_at', '2025-01-22 20:00:00+02');
 	await resetTable('ship_log', shipLogDump, 'created_at', '2024-07-05 00:07:00.000+03');
 	await resetTable('vote', voteDump, 'active_until', '2024-07-05 14:00:00.000+03'); // Cascade deletes also vote_entry
 	await resetTable('vote_option', voteOptionDump);
 	await resetTable('vote_entry', voteEntryDump);
+	await resetTable('medical_entry', []);
+	await resetTable('operation_result', []);
+	await resetTable('person_entry', personEntryDump);
 }
